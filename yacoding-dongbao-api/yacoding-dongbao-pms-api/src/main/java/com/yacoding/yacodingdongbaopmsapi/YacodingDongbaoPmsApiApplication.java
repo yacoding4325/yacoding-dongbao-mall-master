76884f9dc075976d468f63e40b7684e3b4ed0ea4
yxym0203 <1500081802@qq.com>
2022-12-13 19:50:06 +0800

    商品信息 的展现

---
+++ yacoding-dongbao-api/yacoding-dongbao-pms-api/src/main/java/com/yacoding/yacodingdongbaopmsapi/YacodingDongbaoPmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.yacoding.yacodingdongbaopmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YacodingDongbaoPmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YacodingDongbaoPmsApiApplication.class, args);
+	}
+
+}
---
+++ yacoding-dongbao-service/yacoding-dongbao-pms/src/main/java/com/yacoding/yacodingdongbaopms/YacodingDongbaoPmsApplication.java
@@ -0,0 +1,19 @@
+package com.yacoding.yacodingdongbaopms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author yaCoding
+ * @create 2022-12-13 下午 3:51
+ */
+
+@SpringBootApplication
+public class YacodingDongbaoPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YacodingDongbaoPmsApplication.class, args);
+
+    }
+
+}
