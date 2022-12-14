53662875dab0bbcaa2a81f245fa491f7630ffa9a
yxym0203 <1500081802@qq.com>
2022-12-14 21:50:08 +0800

    ...

---
+++ yacoding-dongbao-api/yacoding-dongbao-cart-api/src/main/java/com/yacoding/yacodingdongbaocartapi/YacodingDongbaoCartApiApplication.java
@@ -0,0 +1,17 @@
+package com.yacoding.yacodingdongbaocartapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author yaCoding
+ * @create 2022-12-14 下午 9:48
+ */
+@SpringBootApplication
+public class YacodingDongbaoCartApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YacodingDongbaoCartApiApplication.class,args);
+    }
+
+}
